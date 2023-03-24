8f137175b4550e827754876e3f19d09a714a29a9
Pravin <0131061@kdu-online.com>
2023-03-24 12:20:12 +0800

    updated todo to allow enter key to get input

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -6,23 +6,33 @@ import './Todo.css';
     const [todo, setTodo] = useState("");
     const [todos, setTodos] = useState([]); 
 
+//TO ADD TODO ITEM
 const addTodo = () => {
      if (todo.trim().length!==0) { 
         setTodos([...todos, todo]); 
         setTodo("");
+        console.log("New task has been added");
      } else{
         alert('please enter a task')
      }
 };
+//TO REMOVE TODO ITEM
  const removeTodo = (index) => { 
     const updatedTodos = todos.filter((_, i) => i !== index); 
     setTodos(updatedTodos); 
+};
+//TO ALLOW USER TO USE ENTER KEY 
+const handleEnterKey = (e) =>{
+    if (e.key ==="Enter"){
+        addTodo(e);
+        console.log("enter key is pressed");
+    }
 };
  return ( 
      <div className="todo">
          <h1>Todo List</h1> 
          <div>
-         <input className="input" type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+         <input className="input" type="text" value={todo} onChange={(e) => setTodo(e.target.value)} onKeyDown={handleEnterKey} />
          </div>
          
          <button className="button" onClick={addTodo}>Add Todo</button> 
