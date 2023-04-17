28983b79f1a15171919bc389862462ae29334cf8
Pravin <0131061@kdu-online.com>
2023-04-17 10:24:40 +0800

    import path edit

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, Suspense, useEffect} from "react";
 import ReactDOM from "react-dom";
 import './Todo.css';
+
+const Calculatorcom = React.lazy(()=> import("calculatormf/Calculator")) ;
 //import Calculator from "../../../calculatormf/src/Calculator";
-const Calculator = React.lazy(()=> import("../../calculatormf/src/Calculator")) ;
 // import { Calculator } from "calculatormf/Calculator";
 
 // import Header from "../../../homemf/src/Header";
 // import Footer from "../../../homemf/src/Footer";
 // import UserForm from "../../../homemf/src/userForms";
 
- const TodoList = () => { 
+const TodoList = () => { 
     const [todo, setTodo] = useState("");
     const [todos, setTodos] = useState([]); 
+//     const [Calculator, setCalculator] = useState(null);
+//  useEffect(() => {
+       
+//          // Dynamically import the Calculator component
+//          import("http://localhost:3002/remoteEntry.js")
+//            .then((remote) => remote.calculatormf["./Calculator"])
+//            .then((Calculator) => {
+//              // Set the Calculator component in the state
+//              setCalculator(Calculator);
+//              console.log('calculator component called ')
+//            })
+//            .catch((error) => {
+//              // Handle any errors
+//              console.error(error);
+//              console.log('error emited')
+//            });
+//        }, []);
+     
+//        if (!Calculator) {
+//          // Render a loading state while the Calculator component is being loaded
+//          return <div>Loading...</div>;
+//        }
 
 //TO ADD TODO ITEM
 const addTodo = () => {
@@ -54,7 +77,9 @@ const handleEnterKey = (e) =>{
             </span>
             
         </ul> 
-        <Calculator/>
+        <Suspense fallback={<span>Loading...</span>}>
+         <Calculatorcom/>
+        </Suspense>
        
       </div> 
     ); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,190 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
 const deps = require("./package.json").dependencies;
+
 module.exports = {
-  output: {
-    publicPath: "http://localhost:3000/",
-  },
-
+  mode: "development",
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    extensions: [".css", ".scss", ".js", ".jsx"],
   },
-
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-  },
-
   module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
+        rules: [
+          {
+            test: /\.s?css$/,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: {
+                  url: {
+                    filter: (url) => {
+                      if (url.startsWith("data:")) {
+                        return false;
+                      }
+                      return true;
+                    },
+                  },
+                },
+              },
+              "sass-loader",
+            ],
+          },
+          {
+            test: /\.jsx?$/,
+            use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react", "@babel/preset-env"],
+            }},
+            exclude: /node_modules/,
+          },
+        ],
       },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-          }
-
-        },
-      },
-    ],
-  },
-
   plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
     new ModuleFederationPlugin({
       name: "rxjsdashboard",
       filename: "remoteEntry.js",
       remotes: {
-        homemf: "homemf@http://localhost:3003/remoteEntry.js",
         calculatormf: "calculatormf@http://localhost:3002/remoteEntry.js"
       },
-      exposes: {},
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
     }),
   ],
 };
+
+
+//'''''''''''''''''''''''''''''''''''''''''''
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const path = require("path");
+// const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+// const deps = require("./package.json").dependencies;
+
+// module.exports = {
+//   mode: "development",
+//   resolve: {
+//     extensions: [".css", ".scss", ".js", ".jsx"],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.s?css$/,
+//         use: [
+//           "style-loader",
+//           {
+//             loader: "css-loader",
+//             options: {
+//               url: {
+//                 filter: (url) => {
+//                   if (url.startsWith("data:")) {
+//                     return false;
+//                   }
+//                   return true;
+//                 },
+//               },
+//             },
+//           },
+//           "sass-loader",
+//         ],
+//       },
+//       {
+//         test: /\.jsx?$/,
+//         use: {
+//         loader: "babel-loader",
+//         options: {
+//           presets: ["@babel/preset-react", "@babel/preset-env"],
+//         }},
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: path.resolve(__dirname, "public", "index.html"),
+//     }),
+//     new ModuleFederationPlugin({
+//       name: "rxjsdashboard",
+//       filename: "remoteEntry.js",
+//       remotes: {
+//         calculatormf: "calculatormf@http://localhost:3002/remoteEntry.js"
+//       },
+//     }),
+//   ],
+// };
+
+//'''''''''''''''''''''''''''''''''''''''''''''''''''''''
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+// const deps = require("./package.json").dependencies;
+// module.exports = {
+//   mode : 'development',
+//   output: {
+//     publicPath: "http://localhost:3000/",
+//   },
+
+//   resolve: {
+//     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+//   },
+
+//   devServer: {
+//     port: 3000,
+//     historyApiFallback: true,
+//   },
+
+//   module: {
+//     rules: [
+//       {
+//         test: /\.m?js/,
+//         type: "javascript/auto",
+//         resolve: {
+//           fullySpecified: false,
+//         },
+//       },
+//       {
+//         test: /\.(css|s[ac]ss)$/i,
+//         use: ["style-loader", "css-loader", "postcss-loader"],
+//       },
+//       {
+//         test: /\.(ts|tsx|js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["@babel/preset-react", "@babel/preset-env"],
+//           }
+
+//         },
+//       },
+//     ],
+//   },
+
+//   plugins: [
+//     new ModuleFederationPlugin({
+//       name: "rxjsdashboard",
+//       filename: "remoteEntry.js",
+//       remotes: {
+//         homemf: "homemf@http://localhost:3003/remoteEntry.js",
+//         calculatormf: "calculatormf@http://localhost:3002/remoteEntry.js"
+//       },
+//       exposes: {},
+//       shared: {
+//         ...deps,
+//         react: {
+//           singleton: true,
+//           requiredVersion: deps.react,
+//         },
+//         "react-dom": {
+//           singleton: true,
+//           requiredVersion: deps["react-dom"],
+//         },
+//       },
+//     }),
+//     new HtmlWebPackPlugin({
+//       template: "./public/index.html",
+//     }),
+//   ],
+// };
 
